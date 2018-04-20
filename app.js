f77d71cf85bfbbd3f75feace483c7fb067c78177
Marco Velaśquez <velasquez.marco@outlook.com>
2018-04-20 12:25:25 -0400

    Ya resuelve localhost:3000

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//Modificado por MarkitoV
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var index = require('./routes/index');
+
+var service = require('./routes/api/v1.0/service');
 
 var app = express();
 
@@ -21,14 +26,18 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//Modificado por MarkitoV
+//app.use('/', routes);
+//app.use('/users', users);
+
+app.use('/', index);
+app.use('/api/v1.0/', service);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -36,24 +45,29 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
 module.exports = app;
+
+var port = 3000;
+app.listen(port, () => {
+  console.log("Servidor corriendo en " + port)
+});
